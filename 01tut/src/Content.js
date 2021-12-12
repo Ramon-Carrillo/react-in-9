6638e9d98af85f9661c03d7d8ea6cfd473b52699
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-12-11 23:25:40 -0600

    added lists, keys, and css

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,12 +1,62 @@
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+
 const Content = () => {
-  const handleNameChange = () => {
-    const names = ['Bob', 'George', 'John'];
-    const int = Math.floor(Math.random() * names.length);
-    return names[int];
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: 'item1',
+    },
+    {
+      id: 2,
+      checked: false,
+      item: 'Item 2',
+    },
+    {
+      id: 3,
+      checked: false,
+      item: 'Item 3',
+    },
+  ]);
+
+  const handleChecked = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item,
+    );
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
   };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  };
+
   return (
     <main>
-      <p>Hello {handleNameChange()}</p>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className='item' key={item.id}>
+              <input
+                type='checkbox'
+                onChange={() => handleChecked(item.id)}
+                checked={item.checked}
+              />
+              <label
+                onDoubleClick={() => handleChecked(item.id)}
+                style={item.checked ? { textDecoration: 'line-through' } : null}>
+                {item.item}
+              </label>
+              <FaTrashAlt onClick={() => handleDelete(item.id)} role='button' tabIndex='0' />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty</p>
+      )}
     </main>
   );
 };
