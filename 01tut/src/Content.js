23e8d5646a05de1502c2dca3db895e78b98bf514
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-12-13 19:31:00 -0600

    added json server and fetch api data.

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -2,13 +2,13 @@ import ItemList from './ItemList';
 
 const Content = ({ items, handleChecked, handleDelete }) => {
   return (
-    <main>
+    <>
       {items.length ? (
         <ItemList items={items} handleChecked={handleChecked} handleDelete={handleDelete} />
       ) : (
         <p style={{ marginTop: '2rem' }}>Your list is empty</p>
       )}
-    </main>
+    </>
   );
 };
 
